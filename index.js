a4aacc61ee0f1e07be0c93996725109a3856815b
Avleen Kaur <avleen2898@gmail.com>
2018-06-25 13:40:01 -0400

    Basic homework - Clicking button changes heading

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('It works');
+const button = document.querySelector('button');
+
+const changeHeading = function()
+{
+    const heading = document.querySelector('h1');
+    heading.textContent = "A whole mess of Chrisses making a mess of Christmas?";
+}
+
+button.addEventListener('click', changeHeading);
