9b67d59636328e6944b4da9f0912233f63d3597c
Avleen Kaur <avleen2898@gmail.com>
2018-06-25 13:51:11 -0400

    Bonus credit - Added multiple headings and another button

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const button = document.querySelector('button');
+const button1 = document.querySelector('#btn1');
+const button2 = document.querySelector('#btn2');
 
-const changeHeading = function()
-{
-    const heading = document.querySelector('h1');
-    heading.textContent = "A whole mess of Chrisses making a mess of Christmas?";
+const changeHeading1 = function(){
+    const heading1 = document.querySelector('h1');
+    heading1.textContent = "A whole mess of Chrisses making a mess of Christmas?";
+}
+const changeHeading2 = function(){
+    const heading2 = document.querySelector('#description');
+    heading2.textContent = "This might just turn into the messiest Christmas mess time?";
 }
 
-button.addEventListener('click', changeHeading);
+button1.addEventListener('click', changeHeading1);
+button2.addEventListener('click', changeHeading2);
