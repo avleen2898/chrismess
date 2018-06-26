e7d6f7d04e7daa6f34b68fbeaadc9653193aab9c
Avleen Kaur <avleen2898@gmail.com>
2018-06-26 13:34:01 -0400

    Update function name and display form fields in a list

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-const button1 = document.querySelector('#btn1');
-const button2 = document.querySelector('#btn2');
+const form = document.querySelector("#chrisMovies");
 
-const changeHeading1 = function(){
-    const heading1 = document.querySelector('h1');
-    heading1.textContent = "A whole mess of Chrisses making a mess of Christmas?";
-}
-const changeHeading2 = function(){
-    const heading2 = document.querySelector('#description');
-    heading2.textContent = "This might just turn into the messiest Christmas mess time?";
-}
-const changeHeadingFromForm = function(){
-    const newHeading = document.querySelector('#newHeading');
-    const heading1 = document.querySelector('h1');
-    heading1.textContent = newHeading.value;
+const listOfMovies = function(ev){
+    ev.preventDefault();
+    const list = document.querySelector('#displayMovies');
+    let item = document.createElement('li');
+    let movieName = document.querySelector('#movieName').value;
+    let movieYear = document.querySelector('#movieYear').value;
+    item.textContent = movieName + ' ' + movieYear;
+    list.appendChild(item);
+    form.reset();
 }
 
-button1.addEventListener('click', changeHeading1);
-button2.addEventListener('click', changeHeading2);
+form.addEventListener('submit', listOfMovies);
 
-document.querySelector("#submit").addEventListener('click', function(event) {
-    event.preventDefault();
-}, false);
 
-document.querySelector("#submit").addEventListener('click', changeHeadingFromForm);
 
