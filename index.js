14106b8847b0637adc29f4ded5040d74ba2166ce
Avleen Kaur <avleen2898@gmail.com>
2018-06-26 21:47:14 -0400

    Divide movie name and movie release year into separate lists

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const form = document.querySelector("#chrisMovies");
 
-const generateNewElement = function(){
-    let item = document.createElement('li');
+const generateNewMovie = function(){
+    let movie = document.createElement('li');
     let movieName = document.querySelector('#movieName').value;
+    movie.textContent = movieName;
+    return movie;
+}
+const generateNewYear = function(){
+    let year = document.createElement('li');
     let movieYear = document.querySelector('#movieYear').value;
-    item.textContent = movieName + ' ' + movieYear;
-    return item;
+    year.textContent = movieYear;
+    return year;
 }
 const listOfMovies = function(ev){
     ev.preventDefault();
-    const list = document.querySelector('#displayMovies');
-    list.appendChild(generateNewElement());
+    const list1 = document.querySelector('#displayMovies');
+    const list2 = document.querySelector('#displayYears');
+    list1.appendChild(generateNewMovie());
+    list2.appendChild(generateNewYear());
     form.reset();
 }
 
