2567909a861adeac8a900d9af4fc9496547e9406
Avleen Kaur <avleen2898@gmail.com>
2018-06-27 16:14:16 -0400

    Add some comments to review the progress made

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 class App{
-
     constructor(){
         const form = document.querySelector("#chrisMovies");
         form.addEventListener('submit', (ev) => {
@@ -9,6 +8,7 @@ class App{
         this.flicks = [];
     }
 
+    //Create a function that creates a span element and adds a class and text content to it
     renderProperty(name, value){
         const span = document.createElement('span');
         span.classList.add(name);
@@ -17,51 +17,60 @@ class App{
     }
 
     renderItem(flick){
+
+        //Create a list item to be added to the unordered list
         const item = document.createElement('li');
         //item.classList.add('flick');
 
+        //Create an array of all the keys in the flick object 
         const properties = Object.keys(flick);
 
+        //For each of the keys, render the property
         properties.forEach((propertyName) => {
             const span = this.renderProperty(propertyName, flick[propertyName]);
             item.appendChild(span);
         });
-
         return item;
-
     }
 
     handleSubmit(ev){
+
         const f = ev.target;
 
+        //Creat a flick object that stores the name and year of the movie typed in the form field
         const flick = {
             movieName: f.movieName.value,
             movieYear: f.movieYear.value,
         };
         
+        //Push the flick object onto the flicks array 
         this.flicks.push(flick);
-        console.log(this.flicks);
        
         const item = this.renderItem(flick);
         const list = document.querySelector('#flicks');
         
+        //Append the list item to the unordered list 
         list.appendChild(item);
+
+        //Create a button element and append it to each of the list items
         const deleteButton = document.createElement('button');
         deleteButton.textContent = 'Delete item';
         item.appendChild(deleteButton);
 
         deleteButton.addEventListener('click', () =>{
+            //Delete all the child nodes (span and button) of the list item
             while (item.hasChildNodes()) {
                 item.removeChild(item.firstChild);
             }
+            //Delete the list item from the unordered list
             list.removeChild(item);
+            //Find the index of the flick object and delete it from the flicks array
             let i = this.flicks.indexOf(flick);
             this.flicks.splice(i,1);
         });
         
         f.reset();
         f.movieName.focus();
-
     }
 }
 
